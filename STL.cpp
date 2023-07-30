0b6ac8f67cb07cecc46f9cdcdfc16da168128069
Rajeev Tapadia <rtapadia730@gmail.com>
2023-07-31 01:05:50 +0530

    stl

---
+++ STL.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    // syntax:
+    // [classname]<data_type> [object_name] = values;
+    vector<int> v1 = {1,3,4,5,6};
+
+    // doesn't work like this
+    // vector v2 = new vector();
+}
