81015455aaf8cea292ddf1df0fc2a1ed42461b93
Rajeev Tapadia <rtapadia730@gmail.com>
2023-08-01 17:37:09 +0530

    inorder non-recursive

--- BT.cpp
+++ BT.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 class node{
-    private:
+    public:
     int data;
     node* left;
     node* right;
 
-    public:
     node(int input){
         this->left = NULL;
         this->right = NULL;
@@ -19,15 +19,50 @@ class node{
 };
 
 class binaryTree{
-    private:
+    public:
     node* root;
 
-    public:
     binaryTree(int data){
         this->root = new node(data);
     }
+    node* insertLevelOrder(node* root, vector<int> arr, int index, int arrSize){
+        if(index<arrSize){
+            node* temp = new node(arr.at(index));
+            root = temp;
+        root->left = insertLevelOrder(root->left, arr, (2*index+1), arrSize);
+        root->right = insertLevelOrder(root->right, arr, (2*index+2), arrSize);
+        }
+        return root;
+    }
+    void inorderNR(){
+        if(this->root == NULL){
+            cout<<"Tree is empty"<<endl;
+            return;
+        }
+        stack<node*> st;
+        node* current = this->root;
+        while(current != NULL || !st.empty()){
+            while(current != NULL){
+                st.push(current);
+                current = current->left;
+            }
+            current = st.top();
+            st.pop();
+            cout<<current->data<<" ";
+            current = current->right;
+        }
+        cout<<endl;
+    }
+
 };
 
-void main(){
+int main(){
     binaryTree *bt = new binaryTree(1);
+
+    vector<int> arr = {1,2,3,4,5,6,7};
+    bt->root = NULL;
+    bt->root = bt->insertLevelOrder(bt->root, arr, 0, arr.size());
+
+    bt->inorderNR();
+    return 0;
 }
--- STL.cpp
+++ STL.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <array>
 
 using namespace std;
 
 int main(){
+    array<int, 6> arr = {1,2,4,5,6,7};
+    int len = arr.size();
+    cout<<len<<endl;
     // syntax:
     // [classname]<data_type> [object_name] = values;
-    vector<int> v1 = {1,3,4,5,6};
+    // vector<int> v1 = {1,3,4,5,6};
 
-    // doesn't work like this
-    // vector v2 = new vector();
+    // // doesn't work like this
+    // // vector v2 = new vector();
 
-    v1.push_back(30);
-    v1.push_back(34);
+    // v1.push_back(30);
+    // v1.push_back(34);
 
-    // v1.pop_back();
+    // // v1.pop_back();
 
-    // this pointer and the i object is not the same
-    // vector<int> *itr;
-    vector<int>::iterator i;
+    // // this pointer and the i object is not the same
+    // // vector<int> *itr;
+    // vector<int>::iterator i;
 
-    for(i = v1.begin(); i != v1.end(); i++)
-        cout<<++*i<<endl;
+    // for(i = v1.begin(); i != v1.end(); i++)
+    //     cout<<++*i<<endl;
         
-    // for each loop
-    // for(int j:v1)
-    //     cout<<j<<endl;
+    // // for each loop
+    // // for(int j:v1)
+    // //     cout<<j<<endl;
 
 
-    // list
-    list<int> l = {1,2,3,4,5,6};
+    // // list
+    // list<int> l = {1,2,3,4,5,6};
 
-    l.push_back(10);
+    // l.push_back(10);
 
-    list<int>::iterator k;
+    // list<int>::iterator k;
 
-    for(k = l.begin(); k != l.end(); k++)
-        cout<<*k<<endl;
+    // for(k = l.begin(); k != l.end(); k++)
+    //     cout<<*k<<endl;
 }
